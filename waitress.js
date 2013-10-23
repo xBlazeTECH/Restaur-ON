ff1190eed58f27c3ee913b7ddbcf4c3e43ce5776
Lansing Nye-Madden <xBlazeTECH@users.noreply.github.com>
2013-10-23 12:36:32 -0400

    Added in Waitstaff, Kitchen, and Admin Dashboards.

--- waitress.js
+++ waitress.js
@@ -24,6 +24,68 @@ app.get('/', function(req, res){
   }
 });
 
+app.get('/waitstaff', function(req, res){
+  var path = __dirname + '/content/dashboard/waitstaff.json';
+    fs.readFile(path, 'utf8', function (err, data) {
+    if (err) {
+      console.log('**ERROR: There was an error while loading homepage content!\n' + err);
+      return;
+    }
+    info = null;
+    info = JSON.parse(data);
+    processNode();
+   });
+  function processNode() {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><head><title>' + info.title + '</title></head>');
+    res.write('<body>');
+    res.write('<h1>' + info.header + '</h1>');
+    res.write(info.content);
+    res.end('</body></html>');
+  }
+});
+
+app.get('/kitchen', function(req, res){
+  var path = __dirname + '/content/dashboard/kitchen.json';
+    fs.readFile(path, 'utf8', function (err, data) {
+    if (err) {
+      console.log('**ERROR: There was an error while loading homepage content!\n' + err);
+      return;
+    }
+    info = null;
+    info = JSON.parse(data);
+    processNode();
+   });
+  function processNode() {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><head><title>' + info.title + '</title></head>');
+    res.write('<body>');
+    res.write('<h1>' + info.header + '</h1>');
+    res.write(info.content);
+    res.end('</body></html>');
+  }
+});
+
+app.get('/admin', function(req, res){
+  var path = __dirname + '/content/dashboard/admin.json';
+    fs.readFile(path, 'utf8', function (err, data) {
+    if (err) {
+      console.log('**ERROR: There was an error while loading homepage content!\n' + err);
+      return;
+    }
+    info = null;
+    info = JSON.parse(data);
+    processNode();
+   });
+  function processNode() {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><head><title>' + info.title + '</title></head>');
+    res.write('<body>');
+    res.write('<h1>' + info.header + '</h1>');
+    res.write(info.content);
+    res.end('</body></html>');
+  }
+});
 /* This is my way of using query. It is for my reference.
 app.get('/hi', function(req, res){
   var body = req.query.body;
