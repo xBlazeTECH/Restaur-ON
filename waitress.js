618a2b7507857e6e113ea1c1fee08cb1aacc6d67
Lansing Nye-Madden <me@xblaze.net>
2013-10-23 20:16:57 +0400

    Removed files no longer nessisary. Modified Waitress.js with Express.

--- waitress.js
+++ waitress.js
@@ -1,102 +1,33 @@
-// Define Requires
 var fs = require('fs');
-var http = require('http');
 var qs = require('querystring');
+var express = require('express');
+var app = express();
 
-// Configuration Stuff
-var waitdashIP = '0.0.0.0';
-var admindashIP = '0.0.0.0';
-
-var serverHOST = '0.0.0.0';
-var waitdashPORT = 12301;
-var admindashPORT = 12302;
-var kitchendashPORT = 12303;
-
-/* Depreciated (Replaced by Individual Pages.)
-var pageTitle = out.title;
-var pageHeader = out.header;
-var pageContent = out.content;
-*/
-
-// Begin Creation of the Waitstaff Dashboard
-console.log('Attempting to start up the Waitstaff Dashboard');
-http.createServer(function (req, res) {
-   var path = __dirname + '/content/dashboard/waitstaff.json';
-   fs.readFile(path, 'utf8', function (err, data) {
-      if (err) {
-         console.log('**ERROR: There was an error while loading homepage content!\n' + err);
-         return;
-      }
-      info = null;
-      info = JSON.parse(data);
-      console.log('Homepage Loaded Sucessfully with');
-      console.dir(info);
-      processNode();
+app.get('/', function(req, res){
+  var path = __dirname + '/content/dashboard/homepage.json';
+    fs.readFile(path, 'utf8', function (err, data) {
+    if (err) {
+      console.log('**ERROR: There was an error while loading homepage content!\n' + err);
+      return;
+    }
+    info = null;
+    info = JSON.parse(data);
+    processNode();
    });
   function processNode() {
-     res.writeHead(200, {'Content-Type': 'text/html'});
-     res.write('<html><head><title>' + info.title + '</title></head>');
-     res.write('<body>');
-     res.write('<h1>' + info.header + '</h1>');
-     res.write(info.content);
-     res.end('</body></html>');
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write('<html><head><title>' + info.title + '</title></head>');
+    res.write('<body>');
+    res.write('<h1>' + info.header + '</h1>');
+    res.write(info.content);
+    res.end('</body></html>');
   }
-}).listen(waitdashPORT, serverHOST);
-console.log('Waitstaff Dashboard Started Sucessfully!');
-//console.log('Waitstaff Dashboard Started at http://' + waitdashIP + ':' + waitdashPORT + '/');
+});
 
+app.get('/hi', function(req, res){
+  var body = 'Bye There!';
+  res.end(body);
+});
 
-// Begin the creation of the Administrative Dashboard
-console.log('Attempting to start up the Administrator Dashboard.');
-
-http.createServer(function (req, res) {
-   var path = __dirname + '/content/dashboard/admin.json';
-   fs.readFile(path, 'utf8', function (err, data) {
-      if (err) {
-         console.log('**ERROR: There was an error while loading homepage content!\n' + err);
-         return;
-      }
-      info = null;
-      info = JSON.parse(data);
-      console.log('Homepage Loaded Sucessfully with');
-      console.dir(info);
-      processNode();
-   });
-  function processNode() {
-     res.writeHead(200, {'Content-Type': 'text/html'});
-     res.write('<html><head><title>' + info.title + '</title></head>');
-     res.write('<body>');
-     res.write('<h1>' + info.header + '</h1>');
-     res.write(info.content);
-     res.end('</body></html>');
-  }
-}).listen(admindashPORT, serverHOST);
-
-console.log('Administrative Dashboard Started Sucessfully.');
-
-// Begin the creation of the Kitchen Dashboard
-console.log('Attempting to start up the Kitchen Dashboard.');
-
-http.createServer(function (req, res) {
-   var path = __dirname + '/content/dashboard/kitchen.json';
-   fs.readFile(path, 'utf8', function (err, data) {
-      if (err) {
-         console.log('**ERROR: There was an error while loading homepage content!\n' + err);
-         return;
-      }
-      info = null;
-      info = JSON.parse(data);
-      console.log('Homepage Loaded Sucessfully with');
-      console.dir(info);
-      processNode();
-   });
-  function processNode() {
-     res.writeHead(200, {'Content-Type': 'text/html'});
-     res.write('<html><head><title>' + info.title + '</title></head>');
-     res.write('<body>');
-     res.write('<h1>' + info.header + '</h1>');
-     res.write(info.content);
-     res.end('</body></html>');
-  }
-}).listen(kitchendashPORT, serverHOST);
-console.log('Kitchen Dashboard Started Sucessfully');
+app.listen(3000);
+console.log('Listening on port 3000');
