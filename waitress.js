f1329c11f7cf8ebe9c91684477ed97b72822d849
Lansing Nye-Madden <me@xblaze.net>
2013-10-30 20:23:14 +0400

    Many Changes - Auth works, Cookies works, and more...

--- waitress.js
+++ waitress.js
@@ -8,6 +8,7 @@ var express = require('express');
 var app = express();
 app.use(connect.bodyParser());
 app.use(express.cookieParser());
+
 //app.use(express.session());
 app.use(express.session({secret: "alphaalpha"}));
 
@@ -15,7 +16,28 @@ app.use(express.session({secret: "alphaalpha"}));
 
 
 app.get('/', function(req, res){
+
   res.writeHead(200, {'Content-Type': 'text/html'});
+
+  if(req.session.areload) {
+    if(req.session.areload == "true") {
+      res.write("<head>");
+      res.write("<!--AutoReload is ON-->");
+      res.write('<meta http-equiv="refresh" content="5"/>');
+      res.write('');
+    }
+  } else {
+
+    var array = fs.readFileSync(__dirname + '/system/content/util/waitstaff-login.html').toString().split("\n");
+    for(i in array) {
+        res.write(array[i]);
+    }
+  }
+  
+  var array = fs.readFileSync(__dirname + '/system/content/util/autorefresh.html').toString().split("\n");
+  for(i in array) {
+      res.write(array[i]);
+  }
   
   var array = fs.readFileSync(__dirname + '/system/content/blocks/header.html').toString().split("\n");
   for(i in array) {
@@ -23,11 +45,6 @@ app.get('/', function(req, res){
   }
   
   var array = fs.readFileSync(__dirname + '/system/content/blocks/mainmenu.html').toString().split("\n");
-  for(i in array) {
-      res.write(array[i]);
-  }
-  
-  var array = fs.readFileSync(__dirname + '/system/content/blocks/sidemenu.html').toString().split("\n");
   for(i in array) {
       res.write(array[i]);
   }
@@ -41,6 +58,76 @@ app.get('/', function(req, res){
 });
 
 app.get('/waitstaff', function(req, res){
+
+  res.writeHead(200, {'Content-Type': 'text/html'});
+ 
+  var array = fs.readFileSync(__dirname + '/system/content/blocks/header.html').toString().split("\n");
+  for(i in array) {
+      res.write(array[i]);
+  }
+
+  var array = fs.readFileSync(__dirname + '/system/content/blocks/mainmenu.html').toString().split("\n");
+  for(i in array) {
+      res.write(array[i]);
+  }
+
+  if(req.session.wauth) {
+    res.write("<div class='span9' style='padding-top:75px;'>");
+    res.write('You are logged in as: ' + req.session.wauth + '. ');
+    res.write('</div>');
+
+  } else {
+
+    var array = fs.readFileSync(__dirname + '/system/content/util/waitstaff-login.html').toString().split("\n");
+    for(i in array) {
+        res.write(array[i]);
+    }
+  }
+  
+  res.end('</body></html>');
+});
+
+app.post('/waitstaff', function(req, res){
+  var pin = req.body.pin;
+  var profileLoc = __dirname + '/system/pins/' + pin + '.json';
+  
+  fs.readFile(profileLoc, 'utf8', function (err, data) {
+    console.log('Waitstaff Authentication Recieved');
+    console.log('Authenticating Pin: ' + req.body.pin);
+    if (err) {
+      console.log('Pin was not found!!');
+      console.log('Displaying Authentication Failure Notice!');
+      res.write('Your Username or Password was Incorrect!');
+      res.redirect('/authfail');
+    } else {
+      console.log('We found a profile for the user!');
+      var userInfo = JSON.parse(data);
+      authenticate(userInfo);
+    }
+  });
+
+  function authenticate(userInfo) {   
+    req.session.wauth = req.body.pin;
+  
+    var array = fs.readFileSync(__dirname + '/system/content/blocks/header.html').toString().split("\n");
+    for(i in array) {
+        res.write(array[i]);
+    }
+    
+    var array = fs.readFileSync(__dirname + '/system/content/blocks/mainmenu.html').toString().split("\n");
+    for(i in array) {
+        res.write(array[i]);
+    }
+      res.write("<div class='span9' style='padding-top:75px;'>");
+      res.write('<p>Welcome back ' + userInfo.fullname + '!</p>');
+      res.write('<p>You were born on the date: ' + userInfo.dateofbirth + '!</p>');
+      res.write('</div>');
+    
+    res.end('</body></html>');
+  }
+});
+
+app.get('/authfail', function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   
   var array = fs.readFileSync(__dirname + '/system/content/blocks/header.html').toString().split("\n");
@@ -53,12 +140,7 @@ app.get('/waitstaff', function(req, res){
       res.write(array[i]);
   }
   
-  var array = fs.readFileSync(__dirname + '/system/content/blocks/sidemenu.html').toString().split("\n");
-  for(i in array) {
-      res.write(array[i]);
-  }
-  
-  var array = fs.readFileSync(__dirname + '/system/content/dashboard/waitstaff.html').toString().split("\n");
+  var array = fs.readFileSync(__dirname + '/system/content/util/waitstaff-login-failed.html').toString().split("\n");
   for(i in array) {
       res.write(array[i]);
   }
@@ -79,11 +161,6 @@ app.get('/kitchen', function(req, res){
       res.write(array[i]);
   }
   
-  var array = fs.readFileSync(__dirname + '/system/content/blocks/sidemenu.html').toString().split("\n");
-  for(i in array) {
-      res.write(array[i]);
-  }
-  
   var array = fs.readFileSync(__dirname + '/system/content/dashboard/kitchen.html').toString().split("\n");
   for(i in array) {
       res.write(array[i]);
@@ -91,6 +168,8 @@ app.get('/kitchen', function(req, res){
   
   res.end('</body></html>');
 });
+
+//BEGIN Administrator Login Functions
 
 app.get('/admin', function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
@@ -110,33 +189,7 @@ app.get('/admin', function(req, res){
       res.write(array[i]);
   }
   
-  var array = fs.readFileSync(__dirname + '/system/content/admin/admin.html').toString().split("\n");
-  for(i in array) {
-      res.write(array[i]);
-  }
-  
-  res.end('</body></html>');
-});
-
-app.get('/login', function(req, res){
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  
-  var array = fs.readFileSync(__dirname + '/system/content/blocks/header.html').toString().split("\n");
-  for(i in array) {
-      res.write(array[i]);
-  }
-  
-  var array = fs.readFileSync(__dirname + '/system/content/blocks/mainmenu.html').toString().split("\n");
-  for(i in array) {
-      res.write(array[i]);
-  }
-  
-  var array = fs.readFileSync(__dirname + '/system/content/blocks/sidemenu.html').toString().split("\n");
-  for(i in array) {
-      res.write(array[i]);
-  }
-  
-  var array = fs.readFileSync(__dirname + '/system/content/util/login.html').toString().split("\n");
+  var array = fs.readFileSync(__dirname + '/system/content/util/admin-login.html').toString().split("\n");
   for(i in array) {
       res.write(array[i]);
   }
@@ -148,7 +201,7 @@ app.post('/logout', function(req, res){
   res.clearCookie('loggedin');
 });
 
-app.post('/login', function(req, res){
+app.post('/admin/login', function(req, res){
   var usernameIn = req.body.user;
   var passwordIn = req.body.pass;
   var profileLoc = __dirname + '/system/users/' + usernameIn + '.json';
@@ -188,14 +241,61 @@ app.post('/login', function(req, res){
     }
   
     if (usernameIn == "root" && passwordIn == "pass") {
-      res.send('Authentication Sucessful! You are now logged in as ' + req.body.user + '!');
+      res.write('Authentication Sucessful! You are now logged in as ' + req.body.user + '!');
     } else {
-      res.send('Your Username or Password was Incorrect!')
+      res.write('Your Username or Password was Incorrect!')
     }
     
     res.end('</body></html>');
   }
 });
+
+//END Administrator Login Functions
+
+
+//BEGIN Real File Readouts
+
+app.get('/css', function(req, res){
+  var filename = req.query.q;
+  var theme = req.query.theme;
+  var array = fs.readFileSync(__dirname + '/system/content/themes/' + theme + '/' + filename + '.css').toString().split("\n");
+  for(i in array) {
+      res.write(array[i]);
+  }
+  res.end();
+});
+
+app.get('/img', function(req, res){
+  var filename = req.query.q;
+  var extention = req.query.ext;
+  var array = fs.readFileSync(__dirname + '/system/content/img/' + filename + '.' + extention).toString().split("\n");
+  for(i in array) {
+      res.write(array[i]);
+  }
+  res.end();
+});
+
+app.get('/js', function(req, res){
+  var filename = req.query.q;
+  var array = fs.readFileSync(__dirname + '/system/content/js/' + filename + '.js').toString().split("\n");
+  for(i in array) {
+      res.write(array[i]);
+  }
+  res.end();
+});
+
+app.get('/file', function(req, res){
+  var filename = req.query.q;
+  var extention = req.query.ext;
+  var array = fs.readFileSync(__dirname + '/system/content/files/' + filename + '.' + extention).toString().split("\n");
+  for(i in array) {
+      res.write(array[i]);
+  }
+  res.end();
+});
+
+//END Real File Readouts.
+
 
 /* This is my way of using query. It is for my reference.
 app.get('/hi', function(req, res){
