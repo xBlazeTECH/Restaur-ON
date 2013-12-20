022f8f77d96ae5ab9b2a945d31f5e4aaceeb5a49
Lansing Nye-Madden <me@xblaze.net>
2013-12-20 20:07:36 +0400

    Many changes in the waitstaff control panel.

--- waitress.js
+++ waitress.js
@@ -9,6 +9,7 @@ var app = express();
 app.use(connect.bodyParser());
 app.use(express.cookieParser());
 app.use(express.session({secret: "alphaalpha"}));
+app.use(express.favicon(__dirname + "/system/content/img/favicon.ico"));
 
 var getLogin = function(username) {
     var data = '';
@@ -23,7 +24,16 @@ var getLogin = function(username) {
 	data += '</script>';
 	return data;
   };
-
+  
+  
+var getProfile = function(wid) {
+  var chunk = '';
+  console.log('Retrieving information on: ' + wid);
+  var file = __dirname + '/system/wids/' + wid + '.json';
+  var data = fs.readFileSync(__dirname + '/system/wids/' + wid + '.json');
+  return JSON.parse(data);
+};
+  
 var getPage = function(pagename) {
     var data = '';
     var chunk = fs.readFileSync(__dirname + '/system/content/page/' + pagename + '.html').toString().split("\n");
@@ -56,7 +66,6 @@ app.get('/', function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(getBlock('header'));
   res.write(getBlock('mainmenu'));
-  res.write(getBlock('usermenu'));
   res.write(getPage('homepage'));
   res.write(chgClass('home', 'active'));
   res.write(getLogin(req.session.wid));
@@ -65,25 +74,51 @@ app.get('/', function(req, res){
 });
 
 app.get('/waitstaff', function(req, res){
+ /* if (req.query.t) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(getBlock('header'));
+    res.write(getBlock('mainmenu'));
+	res.write(getPage('waitstaff-table-select'));
+	res.write(getLogin(req.session.wauth));
+	res.write(getBlock('footer'));
+    res.end('</body></html>');
+  } else { */
+    if (!req.session.wauth) {
+      res.redirect('/waitstaff/login');
+      res.end();
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(getBlock('header'));
+      res.write(getBlock('mainmenu'));
+	  res.write(getPage('waitstaff'));
+	  res.write(getLogin(req.session.wauth));
+	  res.write(getBlock('footer'));
+      res.end('</body></html>');
+    }
+  //}
+});
+
+app.get('/waitstaff/login', function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(getBlock('header'));
   res.write(getBlock('mainmenu'));
   res.write(getBlock('usermenu'));
   res.write(getPage('waitstaff-login'));
   res.write(chgClass('waitstaff', 'active'));
-  res.write(getLogin(req.session.wid));
+  res.write(getLogin(req.session.wauth));
   res.write(getBlock('footer'));
   res.end('</body></html>');
 });
 
-app.post('/waitstaff', function(req, res){
-  var wid = req.session.wid;
+app.post('/waitstaff/login', function(req, res){
+  var wid = req.body.wid;
   var profileLoc = __dirname + '/system/wids/' + wid + '.json';
   
   fs.readFile(profileLoc, 'utf8', function (err, data) {
     console.log('Waitstaff Authentication Recieved');
     console.log('Verifying WID: ' + req.body.wid);
     if (err) {
+	  console.log(err);
       console.log('WID was not found!!');
       console.log('Booting the person back to login screen!');
 	  req.session.authfail = 'true';
@@ -91,16 +126,8 @@ app.post('/waitstaff', function(req, res){
     } else {
       console.log('Found a WID match, logging in!');
       var userInfo = JSON.parse(data);
-	  req.session.wauth = req.body.pin;
-      res.write(getBlock('header'));    
-      res.write(getBlock('mainmenu'));
-	  res.write(getBlock('usermenu'));
-      res.write("<div class='span9' style='padding-top:75px;'>");
-      res.write('<p>Welcome back ' + userInfo.fullname + '!</p>');
-      res.write(chgClass('waitstaff', 'active'));
-      res.write(getLogin(req.session.wid));
-      res.write(getBlock('footer'));
-      res.end('</body></html>');
+	  req.session.wauth = req.body.wid;
+      res.redirect('/waitstaff');
   }
  });
 });
@@ -112,7 +139,7 @@ app.get('/authfail', function(req, res){
   res.write(getBlock('usermenu'));
   res.write(getPage('waitstaff-login-failed'));
   res.write(chgClass('waitstaff', 'active'));
-  res.write(getLogin(req.session.wid));
+  res.write(getLogin(req.session.wauth));
   res.write(getBlock('footer'));
   res.end('</body></html>');
 });
@@ -121,10 +148,8 @@ app.get('/kitchen', function(req, res){
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(getBlock('header'));    
   res.write(getBlock('mainmenu'));
-  res.write(getBlock('usermenu'));
   res.write(getPage('kitchen'));
-  res.write(chgClass('waitstaff', 'active'));
-  res.write(getLogin(req.session.wid));
+  res.write(chgClass('kitchen', 'active'));
   res.write(getBlock('footer'));
   res.end('</body></html>');
 });
@@ -136,19 +161,20 @@ app.get('/admin', function(req, res){
   res.write(getBlock('header'));    
   res.write(getBlock('mainmenu'));
   res.write(getBlock('usermenu'));
-  res.write(getBlock('admin-login'));
-  res.write(chgClass('waitstaff', 'active'));
-  res.write(getLogin(req.session.wid));
+  res.write(getPage('admin-login'));
+  res.write(chgClass('admin', 'active'));
+  res.write(getLogin(req.session.admusr));
   res.write(getBlock('footer'));
   res.end('</body></html>');
 });
 
-app.post('/logout', function(req, res){
+app.get('/logout', function(req, res){
+  res.session.wid = null;
   res.session.admusr = null;
-  res.redirect('/admin/login');
+  res.redirect('/');
 });
 
-app.post('/admin/login', function(req, res){
+app.post('/admin', function(req, res){
   var usernameIn = req.body.user;
   var passwordIn = req.body.pass;
   var profileLoc = __dirname + '/system/users/' + usernameIn + '.json';
@@ -167,16 +193,26 @@ app.post('/admin/login', function(req, res){
       res.write(getBlock('mainmenu'));
       res.write(getBlock('usermenu'));
       if (usernameIn == "root" && passwordIn == "pass") {
-        res.write('Authentication Sucessful! You are now logged in as ' + req.body.user + '!');
+        res.write('<div class="span9" style="padding-top:70px;">Authentication Sucessful! You are now logged in as ' + req.body.user + '!');
       } else {
-        res.write('Your Username or Password was Incorrect!');
+        res.write('<div class="span9" style="padding-top:70px;">Your Username or Password was Incorrect!');
       }
-	  res.write(chgClass('waitstaff', 'active'));
+	  res.write(chgClass('admin', 'active'));
       res.write(getLogin(req.session.wid));
 	  res.write(getBlock('footer'));
       res.end('</body></html>');
     }
   });
+});
+
+app.get('/tables/:tnum', function(req,res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.write(getBlock('header'));    
+  res.write(getBlock('mainmenu'));
+  res.write('<div class="span9" style="padding-top:70px;">So you would like to see order details for table: ' + req.params.tnum);
+  res.write(chgClass('waitstaff', 'active'));
+  res.write(getLogin(req.session.wid));
+  res.end('</body></html>');
 });
 
 //END Administrator Login Functions
@@ -197,10 +233,8 @@ app.get('/css', function(req, res){
 app.get('/img', function(req, res){
   var filename = req.query.q;
   var extention = req.query.ext;
-  var array = fs.readFileSync(__dirname + '/system/content/img/' + filename + '.' + extention).toString().split("\n");
-  for(i in array) {
-      res.write(array[i]);
-  }
+  var image = fs.readFileSync(__dirname + '/system/content/img/' + filename + '.' + extention);
+  res.write(image);
   res.end();
 });
 
@@ -224,7 +258,6 @@ app.get('/file', function(req, res){
 });
 
 //END Real File Readouts.
-
 
 /* This is my way of using query. It is for my reference.
 app.get('/hi', function(req, res){
